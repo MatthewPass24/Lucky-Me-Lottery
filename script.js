c7ea781ac197356991a0afe65b46217922c1ee01
MatthewPass24 <matthewpass24@gmail.com>
2024-03-22 10:43:40 -0400

    added event listener to show name and for loop

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ function addName() {
     
     nameInput.value = '' //Clear the input field after adding the name
 }
+
+function displayNames() {
+    const nameList = document.getElementById('nameList')
+    nameList.innerHTML = '' //clear out the previous list items
+
+    for (let i = 0; i < namesArray.length; i++){
+        const name = namesArray[i]
+
+        const li = document.createElement('li')
+        li.className = 'list-group-item'
+
+        const span = document.createElement('span')
+        span.textContent = name
+
+        li.appendChild(span)//adds a span tag nested between every <li> tag (the child)
+        nameList.appendChild(li)
+    }
+
+}
+//Event listener for the button click to add a name
+document.getElementById('addNameBtn').addEventListener('click', addName)
